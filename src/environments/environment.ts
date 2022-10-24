9dfd469d47f6b64b5590b92803ecb12203a90084
Tom <tomwansww@gmail.com>
2022-10-24 11:35:50 +0500

    Rename Upload to  Load

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   buttonNewGameCaption: 'Shuffle',
   buttonPrevImageCaption: 'Prev',
   buttonNextImageCaption: 'Next',
-  buttonUploadCaption: 'Upload',
+  buttonUploadCaption: 'Load',
   buttonDeleteCaption: 'Delete',
   isTest: false,
 };
